c819336f5f3202ea602fe08fcdd6e9a4c855ee57
Damimani <dani.toth2003@gmail.com>
2023-01-10 08:30:23 +0100

    mostmár megy bazdmeg

---
+++ Context.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCraftingTree
+{
+    internal class Context : DbContext
+    {
+
+    }
+
+    class Items
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Type { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+    }
+
+
+
+    class Furnace
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public ICollection<Items> OutputSlot { get; set; }
+        public ICollection<Items> ingredientSlot { get; set; }
+    }
+}
